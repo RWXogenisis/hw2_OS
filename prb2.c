e6ab3f3288df3f71afaf69883f95a4190576832f
RWXogenisis <65536776+RWXogenisis@users.noreply.github.com>
2023-02-19 23:38:10 +0530

    Stable version
    Corrected a few anomalies

--- prb2.c
+++ prb2.c
@@ -9,16 +9,29 @@
 
 int random_gen(){
     srand(time(NULL));
-    int temp = rand()%((HIGH-LOW+1)+LOW);
+    int temp = 0;
+    do{
+    temp = rand()%((HIGH-LOW+1)+LOW);
+    }while(temp==0);
     return temp;
 }
 
 int john(int value, int n){
-    if(n>=value){
+    if(n>=value){ //checking of the other player has reached the target
 
         value = value*random_gen();
         printf("John process %d\n", value);
-        return value;
+
+        if(n<value){ //checking if the current player has reached the target
+            printf("John won the game\n");
+            return (-100);
+        }
+        else{
+            return value;
+        }
+    }
+    else{
+        return (-100);
     }
 }
 
@@ -26,7 +39,18 @@ int michael(int value, int n){
     if(n>=value){
         value*=random_gen();
         printf("Michael process %d\n", value);
-        return value;
+
+        if(n<value){ //checking if after multiplication of the player has reached the target
+            printf("Michael won the game\n");
+            return (-100);
+        }
+        else{
+            return value;
+        }
+    }
+
+    else{ //in case the other player has already reached the target
+        return (-100);
     }
 }
 
@@ -34,46 +58,82 @@ int main(){
     int n;
     scanf("%d", &n);
     int p = 1;
-    pid_t pid;
-    int fd[2];
-    pipe(fd);
+    int x = 1;
+    int chance = 1;
+    int p1[2]; //C=>P
+    int p2[2]; //P=>C
 
-    int chance = 1; 
+    //in case the pipes are not declared
+    if (pipe(p1)==-1){
+        return -1;
+    }
+    if(pipe(p2) == -1){
+        return -10;
+    }
 
-    while(!(p>=n)){
-        switch (pid = fork())
-        {
-        case -1:
-            perror("fork failure");
-            exit(EXIT_FAILURE);
-            break;
+    while(x>0 && p>0){ // run the loop till there is a winner, -100 indicates there is a winner
+        int pid = fork();
 
-        case 0:
-            p = michael(p, n);
-            chance += 1;
-            close(fd[0]); 
-            close(1);
-            dup(fd[1]);
-            write(fd[1], &p, sizeof(int));
-            break;
-        
-        default:
-            close(fd[1]); 
-            close(0); 
-            dup(fd[0]); 
-            int n1 = read(fd[0], &p, sizeof(int));
+        switch(pid){
+            case -1:
+                return -2;
+                break;
 
-            p = john(p, n);
-            chance+=1;
-            wait(NULL);
+            case 0:
+                sleep(2); //this is to allow the parent process to execute completely and pass the value to pipe
+                // close(0);
+                // close(p2[1]);
+                // dup(p2[0]);
+                close(p1[0]); //closing the unused sides of the pipe
+                close(p2[1]);
+
+                if(read(p2[0], &x, sizeof(x)) == -1){ //reading the updated value from pipe, returns -1 if there is invalid value
+                    return -3;
+                }
+
+                if(x != -100){ //this is a flag where we check if there is a winner or not
+                    printf("Received %d\n", x);
+                    x = michael(x, n); //updating the value
+                    chance+=1;
+                    printf("value of x is %d\n", x);
+
+                    if(write(p1[1], &x, sizeof(x)) == -1){
+                        return -4;
+                    }
+                }
+                printf("Sent %d\n", x);
+                //close(p2[0]);
+                //close(p1[1]);
+                break;
+            
+            default:
+            //parent process
+                close(p1[1]); //closing the unused ends of the pipe
+                close(p2[0]);
+
+                if(chance != 1){
+                    if(read(p1[0], &p, sizeof(p)) == -1){
+                        return -5;
+                    }
+                }
+                if(p != -100){ //checking for a clear winner
+                    printf("parent received %d\n", p);
+
+                    p = john(p, n);
+                    chance+=1;
+                    printf("value of p is %d\n", p);
+                    if(write(p2[1], &p, sizeof(p)) == -1){  //writing the new value to the pipe
+                        return -6;
+                    }
+                }
+                //printf("%d\n", p2[1]);
+                printf("parent sent %d\n", p);
+                //close(p1[0]);
+                //close(p2[1]);
+                wait(NULL); //waiting for child process to run to maintain the order
+                //sleep(3);
         }
     }
-
-    if(chance%2 == 0){
-        printf("John won\n");
-    }
-    else{
-        printf("Michale won\n");
-    }
-    return 0;
 }
+
+//there will be junk values printed at the end
